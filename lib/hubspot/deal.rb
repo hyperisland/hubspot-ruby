d5e165bf33b3693376c6ec8064551e5ad4cb89ac
Till Leinen <hello@till.is>
2017-03-07 16:50:22 -0500

    Add method for finding deal by contact

--- lib/hubspot/deal.rb
+++ lib/hubspot/deal.rb
@@ -12,6 +12,7 @@ module Hubspot
     RECENT_UPDATED_PATH = "/deals/v1/deal/recent/modified"
     UPDATE_DEAL_PATH = '/deals/v1/deal/:deal_id'
     ASSOCIATE_DEAL_PATH = '/deals/v1/deal/:deal_id/associations/:OBJECTTYPE?id=:objectId'
+    ASSOCIATED_DEAL_PATH = '/deals/v1/deal/associated/:objectType/:objectId'
 
     attr_reader :properties
     attr_reader :portal_id
@@ -47,7 +48,6 @@ module Hubspot
          Hubspot::Connection.put_json(ASSOCIATE_DEAL_PATH, params: { deal_id: deal_id, OBJECTTYPE: objecttype, objectId: object_ids}, body: {})
        end
  
-
       def find(deal_id)
         response = Hubspot::Connection.get_json(DEAL_PATH, { deal_id: deal_id })
         new(response)
@@ -60,6 +60,17 @@ module Hubspot
       def recent(opts = {})
         response = Hubspot::Connection.get_json(RECENT_UPDATED_PATH, opts)
         response['results'].map { |d| new(d) }
+      end
+      
+      # Find all deals associated to a contact
+      # {http://developers.hubspot.com/docs/methods/deals/get-associated-deals}
+      # @param contact [Hubspot::contact] the contact
+      # @return [Array] Array of Hubspot::Deal records
+      def find_by_contact(contact)
+        path = ASSOCIATED_DEAL_PATH
+        params = { objectType: :contact, objectId: contact.vid }
+        response = Hubspot::Connection.get_json(path, params)
+        response["results"].map { |deal_id| find(deal_id) }
       end
 
     end
